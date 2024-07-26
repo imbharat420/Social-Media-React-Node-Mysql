151ca040d6890429509ea456a20ddea465cc1c42
bharat singh <bharat.singh@devtechnosys.info>
2024-07-26 12:04:40 +0530

    demo-video and logout username

--- frontend/src/components/layout/PrivateLayout.jsx
+++ frontend/src/components/layout/PrivateLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../store/features/AuthSlice'
 function PrivateLayout({ children }) {
     const dispatch = useDispatch()
+    const user = useSelector((state) => state.auth.user)
     const handlelogout = () => {
         dispatch(logout())
     }
@@ -11,7 +12,7 @@ function PrivateLayout({ children }) {
         <div className="p-3">
             <div className="d-flex justify-content-end align-items-center">
                 <button className="btn btn-danger" onClick={handlelogout}>
-                    Logout
+                    {user?.username}, Logout
                 </button>
             </div>
             <Outlet />
